59859c8fdc218e02cc1338a937af9b7b45162508
Mykola Haltiuk <kaljandlux@gmail.com>
2023-05-02 15:09:39 +0200

    mapping wikipedia id to title and back

--- wikimapper/mapper.py
+++ wikimapper/mapper.py
@@ -114,3 +114,37 @@ class WikiMapper:
         results = c.fetchall()
 
         return [e[0] for e in results]
+
+    def wikipedia_id_to_title(self, wikipedia_id: int) -> Optional[str]:
+        """???
+
+        Args:
+            wikipedia_id (int): The Wikipedia ID to map, e.g. `18630637`
+
+        Returns:
+            Optional[str]: If a mapping found for `wikipedia_id`, then return
+                           it, else return `None`.
+        """
+
+        c = self.conn.execute(
+            "SELECT wikipedia_title FROM mapping WHERE wikipedia_id=?", (wikipedia_id,)
+        )
+        result = c.fetchone()
+
+        if result is not None and result[0] is not None:
+            return result[0]
+        else:
+            return None
+
+    def title_to_wikipedia_id(self, page_title: str) -> List[int]:
+        """???
+
+        """
+
+        # no need for `DISTINCT` as `wikipedia_id` is a PRIMARY KEY, thus we have no duplicates there
+        c = self.conn.execute(
+            "SELECT wikipedia_id FROM mapping WHERE wikipedia_title=?", (page_title,)
+        )
+        results = c.fetchall()
+
+        return [e[0] for e in results]
